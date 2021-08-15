986199b9c9cba6780c4d9b7b4c0ec617b2270f58
Sigmund <sigmundchianasta@gmail.com>
2021-08-15 17:55:32 +0800

    Update hippodrome-run-suite.sh

--- suite-of-benchmarks/hippodrome-run-suite.sh
+++ suite-of-benchmarks/hippodrome-run-suite.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 # Run all benchmarks in suite-of-benchmarks
 
-cd /usr/local/hippodrome-tests/suite-of-benchmarks/airline
-java -jar /hippodrome-1.0-jar-with-dependencies.jar --config_file=/usr/local/hippodrome-tests/suite-of-benchmarks/airline/CONFIG.json
+cd airline
+java -jar /hippodrome-1.0-jar-with-dependencies.jar
 cd ../alarmclock
-java -jar /hippodrome-1.0-jar-with-dependencies.jar --config_file=/usr/local/hippodrome-tests/suite-of-benchmarks/alarmclock/CONFIG.json
+java -jar /hippodrome-1.0-jar-with-dependencies.jar
 cd ../buggyprogram
-java -jar /hippodrome-1.0-jar-with-dependencies.jar --config_file=/usr/local/hippodrome-tests/suite-of-benchmarks/buggyprogram/CONFIG.json
+java -jar /hippodrome-1.0-jar-with-dependencies.jar
 cd ../consisitency
-java -jar /hippodrome-1.0-jar-with-dependencies.jar --config_file=/usr/local/hippodrome-tests/suite-of-benchmarks/consisitency/CONFIG.json
+java -jar /hippodrome-1.0-jar-with-dependencies.jar
 cd ../datarace
-java -jar /hippodrome-1.0-jar-with-dependencies.jar --config_file=/usr/local/hippodrome-tests/suite-of-benchmarks/datarace/CONFIG.json
+java -jar /hippodrome-1.0-jar-with-dependencies.jar
 cd ../deadlock-paper
-java -jar /hippodrome-1.0-jar-with-dependencies.jar --config_file=/usr/local/hippodrome-tests/suite-of-benchmarks/deadlock-paper/CONFIG.json
+java -jar /hippodrome-1.0-jar-with-dependencies.jar
 cd ../linkedlist
-java -jar /hippodrome-1.0-jar-with-dependencies.jar --config_file=/usr/local/hippodrome-tests/suite-of-benchmarks/linkedlist/CONFIG.json
+java -jar /hippodrome-1.0-jar-with-dependencies.jar
 cd ../nestedclasses1
-java -jar /hippodrome-1.0-jar-with-dependencies.jar --config_file=/usr/local/hippodrome-tests/suite-of-benchmarks/nestedclasses1/CONFIG.json
+java -jar /hippodrome-1.0-jar-with-dependencies.jar
 cd ../nestedclasses2
-java -jar /hippodrome-1.0-jar-with-dependencies.jar --config_file=/usr/local/hippodrome-tests/suite-of-benchmarks/nestedclasses2/CONFIG.json
+java -jar /hippodrome-1.0-jar-with-dependencies.jar
 cd ../nestedclasses3
-java -jar /hippodrome-1.0-jar-with-dependencies.jar --config_file=/usr/local/hippodrome-tests/suite-of-benchmarks/nestedclasses3/CONFIG.json
+java -jar /hippodrome-1.0-jar-with-dependencies.jar
 cd ../nio-io-library-simplified
-java -jar /hippodrome-1.0-jar-with-dependencies.jar --config_file=/usr/local/hippodrome-tests/suite-of-benchmarks/nio-io-library-simplified/CONFIG.json
+java -jar /hippodrome-1.0-jar-with-dependencies.jar
 cd ../pingpong
-java -jar /hippodrome-1.0-jar-with-dependencies.jar --config_file=/usr/local/hippodrome-tests/suite-of-benchmarks/pingpong/CONFIG.json
+java -jar /hippodrome-1.0-jar-with-dependencies.jar
 cd ../wrongLock
-java -jar /hippodrome-1.0-jar-with-dependencies.jar --config_file=/usr/local/hippodrome-tests/suite-of-benchmarks/wrongLock/CONFIG.json
+java -jar /hippodrome-1.0-jar-with-dependencies.jar
 cd ../wronglock2
-java -jar /hippodrome-1.0-jar-with-dependencies.jar --config_file=/usr/local/hippodrome-tests/suite-of-benchmarks/wronglock2/CONFIG.json
+java -jar /hippodrome-1.0-jar-with-dependencies.jar
